ea78ce5cbc6789c26c15f4414f9d1a7348c0bd30
dhenderson <dhenderson@mediweightloss.com>
2012-10-17 14:43:53 -0400

    new git settings test...

--- src/global_consts.py
+++ src/global_consts.py
@@ -15,8 +15,8 @@ class GlobalConsts(object):
 #    BOARD_HEIGHT = 40
 #    BOARD_WIDTH  = 40
 #
-    BOARD_HEIGHT = 4
-    BOARD_WIDTH  = 4
+    BOARD_HEIGHT = 40
+    BOARD_WIDTH  = 40
 
 #    BOARD_HEIGHT = 512
 #    BOARD_WIDTH  = 512
@@ -25,7 +25,7 @@ class GlobalConsts(object):
 #    BOARD_WIDTH  = int(BOARD_WIDTH  * 0.5)
 
 #    BOARD_CHUNK_SIZE = 16 # MUST be in multiples of 2
-    BOARD_CHUNK_SIZE = 4 # MUST be in multiples of 2
+    BOARD_CHUNK_SIZE = 40 # MUST be in multiples of 2
 
     MIN_ZOOM = 0.5
     MAX_ZOOM = 17.0
