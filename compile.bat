ff6c982a3538882c26caa1bb5f526da079779711
Derek <squeesh@gmail.com>
2013-06-26 23:58:34 -0400

    updated compile scripts to use c++0x instead of c++11

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 
 
-g++ -std=c++11 -c -w -fPIC -IC:\python27\include -LC:\python27\libs -lpython27 src/*.cpp
+g++ -std=c++0x -c -w -fPIC -IC:\python27\include -LC:\python27\libs -lpython27 src/*.cpp
 
-g++ -std=c++11 -shared -w src/extern/all_externs.cpp *.o C:\MinGW\lib\libopengl32.a C:\MinGW\lib\libglu32.a -IC:\python27\include -LC:\python27\libs -lpython27 -lglew32 -lfreeglut -o externs.so
+g++ -std=c++0x -shared -w src/extern/all_externs.cpp *.o C:\MinGW\lib\libopengl32.a C:\MinGW\lib\libglu32.a -IC:\python27\include -LC:\python27\libs -lpython27 -lglew32 -lfreeglut -o externs.so
 
-g++ -std=c++11 src/main.cpp externs.so C:\MinGW\lib\libopengl32.a C:\MinGW\lib\libglu32.a -IC:\python27\include -LC:\python27\libs -lglew32 -lfreeglut -lpython27 -o main.exe
+g++ -std=c++0x src/main.cpp externs.so C:\MinGW\lib\libopengl32.a C:\MinGW\lib\libglu32.a -IC:\python27\include -LC:\python27\libs -lglew32 -lfreeglut -lpython27 -o main.exe
--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-g++ -std=c++11 -c -w -fPIC -g -g3 -I/usr/include/python2.7 -L/usr/lib/python2.7 -lpython27 src/*.cpp 2>&1
+g++ -std=c++0x -c -w -fPIC -g -g3 -I/usr/include/python2.7 -L/usr/lib/python2.7 -lpython27 src/*.cpp 2>&1
 
-g++ -std=c++11 -shared -w -fPIC -g -g3 src/extern/all_externs.cpp *.o -I/usr/include/python2.7 -L/usr/lib/python2.7 -lGL -lGLU -lGLEW -lpython2.7 -lglut -o externs.so 2>&1
+g++ -std=c++0x -shared -w -fPIC -g -g3 src/extern/all_externs.cpp *.o -I/usr/include/python2.7 -L/usr/lib/python2.7 -lGL -lGLU -lGLEW -lpython2.7 -lglut -o externs.so 2>&1
 
-g++ -std=c++11 -w -g -g3 src/main.cpp ./externs.so -I/usr/include/python2.7 -L/usr/lib/python2.7 -lGL -lGLU -lGLEW -lpython2.7 -lglut -o main.o 2>&1
+g++ -std=c++0x -w -g -g3 src/main.cpp ./externs.so -I/usr/include/python2.7 -L/usr/lib/python2.7 -lGL -lGLU -lGLEW -lpython2.7 -lglut -o main.o 2>&1
 
